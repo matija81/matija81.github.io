6237c45802c20dc04f0370497fe95bf744114936
Matija Djurkovic <matijadjurkovic@Matija-Djurkovics-MacBook-Pro-2.local>
2018-09-05 12:36:09 -0700

    A

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://mailchimp.com/maxon150@yahoo.com",
+        url: "./mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
